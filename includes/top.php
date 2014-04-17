ddf915615b71804b957d05ff60ffa77d6ed3399b
thb <tbennett@aii.edu>
2014-04-17 01:21:31 +0000

    added status indication

---
+++ includes/top.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Widgco Widgets Inc.</title>
+  <link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body id="<?php echo $page_id; ?>">
